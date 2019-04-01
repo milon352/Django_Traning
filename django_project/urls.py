0cb12af7346a241cb2ec561a0b217b827d9b1854
miloncs50 <milon.cs50@yahoo.com>
2019-04-01 10:57:55 -0700

    login and logout.

--- django_project/urls.py
+++ django_project/urls.py
@@ -14,11 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+
 from users.views import registion
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('new/', registion, name='user-register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
 ]
